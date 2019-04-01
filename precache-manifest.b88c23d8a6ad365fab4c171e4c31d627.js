899b4de078f06e2035292830ada4c8db09086d57
Ryan <ryan.tseng1@gmail.com>
2019-03-31 20:23:48 -0700

    Updates

---
+++ precache-manifest.b88c23d8a6ad365fab4c171e4c31d627.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4533a1c967a934e137f",
+    "url": "/tourneymap/static/js/runtime~main.b4533a1c.js"
+  },
+  {
+    "revision": "ab5f547189ec38f29f6b",
+    "url": "/tourneymap/static/js/main.ab5f5471.chunk.js"
+  },
+  {
+    "revision": "3c7cc75b5d5cb038f1be",
+    "url": "/tourneymap/static/js/1.3c7cc75b.chunk.js"
+  },
+  {
+    "revision": "ab5f547189ec38f29f6b",
+    "url": "/tourneymap/static/css/main.326e7414.chunk.css"
+  },
+  {
+    "revision": "e13906bca2bbe5e04ef198d1b7ee05fc",
+    "url": "/tourneymap/index.html"
+  }
+];
